55941cc762f1ac7e47612905fdd2a40ef61ce1f1
koala4ka <gvani@ukr.net>
2021-09-30 18:47:50 +0300

    routes

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -3,40 +3,42 @@ package controllers
 import controllers.utils.ControllerUtils
 import daos.TokenDAO
 import models.Permission
-import models.dtos.question.{Credentials, SignUpForm, UserUpdateQuestion}
+import models.dtos.question.UserUpdateQuestion
 import monix.execution.Scheduler
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import services.{JWTService, UserService}
 import services.commands.Impl.CheckPermissionCommand
-import services.{AuthService, JWTService, UserService}
+
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
+
 @Singleton
-class AuthController @Inject()(cc: ControllerComponents,
+class UserController @Inject()(cc: ControllerComponents,
                                jwtService: JWTService,
                                tokenDAO: TokenDAO,
-                               authService: AuthService,
+                               userService: UserService,
                                checkPermissionCommand: CheckPermissionCommand
                               )(implicit ex: ExecutionContext,
                                 sch: Scheduler)
   extends ControllerUtils(cc, jwtService, tokenDAO, checkPermissionCommand) {
 
-  def signIn: Action[AnyContent] = actionWithBody[Credentials] {
-    req =>
-      authService.signIn(req.parsedBody)
-        .map(dto => Ok(Json.toJson(dto)))
-  }(Credentials.signInForm)
+  def getAll: Action[AnyContent] =
+    authorizedActionGET(permission = Permission(name = "View")) { implicit request =>
+      userService.getAll().map {
+        users =>
+          val arrayOfJS = users.map(Json.toJson(_))
+          Ok(Json.toJson(arrayOfJS))
+      }
+    }
 
-  def signUp: Action[AnyContent] =
-    authorizedActionPOST[SignUpForm](permission = Permission(name = "User-Add")) {
+  def updateUser(): Action[AnyContent] =
+    authorizedActionPOST[UserUpdateQuestion](permission = Permission(name = "User-Edit")) {
       req =>
-        authService.signUp(req.parsedBody)
+        userService.update(req.parsedBody)
           .map(dto => Ok(Json.toJson(dto)))
-    }(SignUpForm.signUpForm)
+    }(UserUpdateQuestion.userUpdateQuestion)
 
 }
-
-
-
